b8babed9fc305208ea88dcd1e0794784cc54943f
達哉 阿部 <zeal18300092@c-league.jp>
2019-10-02 11:55:48 +0900

    UnityProjectUplord

---
+++ Ms.Mother/Assets/script/MotherC.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotherC : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Player;
+    [SerializeField]
+    GameObject Huton;
+
+    [SerializeField]
+    float rot = 0,time = 0;
+
+    [SerializeField]
+    bool ck = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if(time>0.5f)
+        {
+            time = 0;
+            ck = !ck;
+        }
+
+        if(ck)
+        {
+            //Huton.transform.rotation = Quaternion.Euler(0, 0, 5);
+            if (rot <= 45)
+            {
+                Huton.transform.Rotate(0, 0, 2.5f);
+                rot += 2.5f;
+            }
+            //Player.transform.Translate(0.05f, 0, 0);
+        }
+        else
+        {
+            //Huton.transform.rotation = Quaternion.Euler(0, 0, -5);
+            if (rot >= -45)
+            {
+                Huton.transform.Rotate(0, 0, -2.5f);
+                rot += -2.5f;
+            }
+            //Player.transform.Translate(-0.05f, 0, 0);
+        }
+    }
+}
---
+++ Ms.Mother/Assets/script/PlayerC.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerC : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.Translate(0.2f, 0, 0);
+            Body.transform.Rotate(0,90,0);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.Translate(-0.2f, 0, 0);
+            Body.transform.Rotate(0, -90, 0);
+        }
+
+    }
+}
