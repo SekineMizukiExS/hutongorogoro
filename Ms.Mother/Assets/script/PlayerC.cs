212c38416584347044b51d896dbc2c4c1a6f8eba
SekineMizuki <mitakin29@gmail.com>
2019-10-07 14:02:51 +0900

    [debug]布団を突き抜けて転がる不具合を修正

--- Ms.Mother/Assets/script/PlayerC.cs
+++ Ms.Mother/Assets/script/PlayerC.cs
@@ -6,24 +6,29 @@ public class PlayerC : MonoBehaviour
 {
     [SerializeField]
     GameObject Body;
-
+    [SerializeField]
+    GameObject HutonObj;
+    Vector3 HutonRight;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if (Input.GetKeyDown(KeyCode.D))
         {
-            transform.Translate(0.2f, 0, 0);
+            HutonRight = HutonObj.transform.right;
+            transform.Translate(0.2f * HutonRight);
             Body.transform.Rotate(0,90,0);
         }
         else if (Input.GetKeyDown(KeyCode.A))
         {
-            transform.Translate(-0.2f, 0, 0);
+            HutonRight = HutonObj.transform.right;
+            transform.Translate(-0.2f * HutonRight);
             Body.transform.Rotate(0, -90, 0);
         }
 
