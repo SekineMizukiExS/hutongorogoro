9217f45ecff27bfd128b916a2952f5884e008cd5
達哉 阿部 <zeal18300092@c-league.jp>
2019-11-27 11:18:11 +0900

    [new]MapEidtor追加

---
+++ Ms.Mother/Assets/MapEditor/Script/EnemyParam.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyParam : MonoBehaviour
+{
+    [SerializeField]
+    public string Type;
+    [SerializeField]
+    public string MeshKey;
+    [SerializeField]
+    public string TexKey;
+    [SerializeField]
+    public string State = "Wait";
+    [SerializeField]
+    public List<MovePointParam> Points;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
