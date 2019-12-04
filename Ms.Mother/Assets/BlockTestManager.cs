cc09d40afc11a8677403d8cc27303fd825f43925
SekineMizuki <mitakin29@gmail.com>
2019-12-04 10:18:02 +0900

    [Update]プロトタイプ２の開発

---
+++ Ms.Mother/Assets/BlockTestManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockTestManager : MonoBehaviour
+{
+    public bool PlayerTurn;
+    public bool EnemyTurn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PlayerTurnEnded()
+    {
+        PlayerTurn = false;
+        EnemyTurn = true;
+    }
+    public void EnemyTurnEnded()
+    {
+        EnemyTurn = false;
+        PlayerTurn = true;
+    }
+}
--- Ms.Mother/Assets/EnemyRotateAround.cs
+++ Ms.Mother/Assets/EnemyRotateAround.cs
@@ -2,9 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RotateAroundTest : MonoBehaviour
+public class EnemyRotateAround : MonoBehaviour
 {
-    PlayerColiderEvents PlColEve;
 
     Vector3 rotatePoint = Vector3.zero;  //回転の中心
     Vector3 rotateAxis = Vector3.zero;   //回転軸
@@ -12,29 +11,27 @@ public class RotateAroundTest : MonoBehaviour
 
     public LayerMask blockLayer;
     public BlockTestManager BTM;
-    public bool BTMoffMODE = false;
 
     float cubeSizeHalf;                  //キューブの大きさの半分
     bool isRotate = false;               //回転中に立つフラグ。回転中は入力を受け付けない
 
-
     void Start()
     {
         cubeSizeHalf = transform.localScale.x / 2f;
-        PlColEve = GetComponent<PlayerColiderEvents>();
     }
 
     void Update()
     {
-        if (BTM.PlayerTurn || BTMoffMODE)
+        if (BTM.EnemyTurn)
         {
-
+            
             //Debug.Log("hol:" + Input.GetAxis("Horizontal"));
             //回転中は入力を受け付けない
             if (isRotate)
                 return;
 
-            if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetAxis("Horizontal") >= 0.1f)
+            int i = Random.Range(0,4);
+            if (i == 1)
             {
                 rotatePoint = transform.position + new Vector3(cubeSizeHalf, -cubeSizeHalf, 0f);
 
@@ -44,17 +41,12 @@ public class RotateAroundTest : MonoBehaviour
                 }
                 else
                 {
-                    RaycastHit rb;
-                    Physics.Raycast(transform.position, new Vector3(1, 0, 0),out rb, 1, blockLayer);
-                    if(rb.collider.tag == "enemy")
-                    {
-                        Destroy(rb.collider.gameObject);
-                    }
                     Debug.Log("ng");
+                    BTM.EnemyTurnEnded();
                     return;
                 }
             }
-            else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetAxis("Horizontal") <= -0.1f)
+            else if (i == 2)
             {
                 rotatePoint = transform.position + new Vector3(-cubeSizeHalf, -cubeSizeHalf, 0f);
                 if (!Physics.Raycast(transform.position, new Vector3(-1, 0, 0), 1, blockLayer))
@@ -63,17 +55,12 @@ public class RotateAroundTest : MonoBehaviour
                 }
                 else
                 {
-                    RaycastHit rb;
-                    Physics.Raycast(transform.position, new Vector3(-1, 0, 0), out rb, 1, blockLayer);
-                    if (rb.collider.tag == "enemy")
-                    {
-                        Destroy(rb.collider.gameObject);
-                    }
                     Debug.Log("ng");
+                    BTM.EnemyTurnEnded();
                     return;
                 }
             }
-            else if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetAxis("Vertical") >= 0.1f)
+            else if (i == 3)
             {
                 rotatePoint = transform.position + new Vector3(0f, -cubeSizeHalf, cubeSizeHalf);
                 if (!Physics.Raycast(transform.position, new Vector3(0, 0, 1), 1, blockLayer))
@@ -82,17 +69,12 @@ public class RotateAroundTest : MonoBehaviour
                 }
                 else
                 {
-                    RaycastHit rb;
-                    Physics.Raycast(transform.position, new Vector3(0, 0, 1), out rb, 1, blockLayer);
-                    if (rb.collider.tag == "enemy")
-                    {
-                        Destroy(rb.collider.gameObject);
-                    }
                     Debug.Log("ng");
+                    BTM.EnemyTurnEnded();
                     return;
                 }
             }
-            else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetAxis("Vertical") <= -0.1f)
+            else if (i == 0)
             {
                 rotatePoint = transform.position + new Vector3(0f, -cubeSizeHalf, -cubeSizeHalf);
                 if (!Physics.Raycast(transform.position, new Vector3(0, 0, -1), 1, blockLayer))
@@ -101,13 +83,8 @@ public class RotateAroundTest : MonoBehaviour
                 }
                 else
                 {
-                    RaycastHit rb;
-                    Physics.Raycast(transform.position, new Vector3(0, 0, -1), out rb, 1, blockLayer);
-                    if (rb.collider.tag == "enemy")
-                    {
-                        Destroy(rb.collider.gameObject);
-                    }
                     Debug.Log("ng");
+                    BTM.EnemyTurnEnded();
                     return;
                 }
             }
@@ -144,9 +121,10 @@ public class RotateAroundTest : MonoBehaviour
         isRotate = false;
         rotatePoint = Vector3.zero;
         rotateAxis = Vector3.zero;
-        BTM.PlayerTurnEnded();
+        BTM.EnemyTurnEnded();
         //PlColEve.PlaySound(0);
 
         yield break;
     }
 }
+
